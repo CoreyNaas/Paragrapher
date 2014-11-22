92b50a6ecc41c8438aee0a8645dd313242762713
Corey Naas <coreynaas@outlook.com>
2014-11-22 13:01:32 -0600

    V.0.0.2
    Program beginning to take shape. Bug remains with instances of getline.

--- ParagrapherV1.cpp
+++ ParagrapherV1.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+string newProject();
+string continueProject();
+void projectWorkspace(string projName);
+
 int main(){
 	
 	cout << "Welcome to Paragrapher!" << endl;
@@ -15,19 +19,11 @@ int main(){
 	do{
 	cin >> menuSelect;
 	if(menuSelect == "new"){ //create new project
-		string newProgName, newProgTitle, newAuthName;
-		
-		cout << "Project file name (no spaces): ";
-			getline(cin, newProgName);
-		cout << "Title of project: ";
-			getline(cin, newProgTitle);
-		cout << "Name of Author: ";
-			getline(cin, newAuthName);
-		
-		//newProject(newProgName, newProgTitle, newAuthName);
-		
+		newProject();
+		break;
 	}else if(menuSelect == "continue"){ //continue new project
-	
+		continueProject();
+		break;
 	}else if(menuSelect == "quit"){ //quits program
 		return 0;
 	}else{ // incorrect input
@@ -35,5 +31,28 @@ int main(){
 	}
 	}while(menuSelect != "new" | menuSelect != "continue");
 
+	cout << "ending...";
 	return 0;
 }
+
+string newProject(){ /*gets data from user about project contents*/
+	string newProgName, newProgTitle, newAuthName;
+
+	cout << "Project file name (no spaces): ";
+		getline(cin, newProgName, '\n'); //Buggy, skips first instance of getline
+	cout << "Title of project: ";
+		getline(cin, newProgTitle, '\n');
+	cout << "Name of Author: ";
+		getline(cin, newAuthName, '\n');
+		
+	/*create new file, insert above data into file*/
+	
+	/*pass newProgName to projectWorkspace to load newly create file and allow
+	user to begin working*/
+}
+
+string continueProject(){
+
+
+}
+
